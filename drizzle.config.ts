553dd824e537083121e7fbab6c1719fc5f70a752
lia-lopes <ame.pistache@gmail.com>
2026-04-14 23:38:57 -0300

    fix:updated CI

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,10 @@ export default defineConfig({
   dialect: "sqlite",
   schema: "./db/schema.ts",
   out: "./drizzle",
+  verbose: true,
+  strict: true,
   dbCredentials: {
     url: process.env.DATABASE_URL || "",
     token: process.env.DATABASE_TOKEN || "",
-  }
+  },
 });
