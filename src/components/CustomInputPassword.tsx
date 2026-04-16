21374482cde367b46a642338699bba4aa401cfc0
mrRiqueRique <riquemribeiro2005@gmail.com>
2026-04-16 04:41:38 -0300

    Telas de login e de cadastro atualizadas

---
+++ src/components/CustomInputPassword.tsx
@@ -0,0 +1,36 @@
+import OpenEye from "@assets/eye.svg"
+import ClosedEye from "@assets/eye-off.svg"
+
+interface CustomInputProps{
+    title: string
+    name: string
+}
+
+// TODO: implementar toggle de visibilidade de senha.
+export default function CustomInputPassword(props: CustomInputProps){
+    return(
+        <div className="relative
+            h-15 w-80
+            justify-center
+            md:w-120 ">
+            <input type="password" placeholder="" required name={props.name} className="
+                focus:outline-none
+                peer
+                h-full w-full rounded-xl
+                pl-3.5 pt-3
+                transition-all duration-150
+                border
+                focus:border-2 focus:border-[#50be91]"/>
+
+            <label className="
+                absolute left-4 bottom-4.5
+                font-bold
+                transition-all duration-150
+                peer-focus:bottom-8.5 peer-focus:text-sm
+                peer-not-placeholder-shown:bottom-8.5 peer-not-placeholder-shown::text-sm
+                text-gray-500">
+                {props.title}
+            </label>
+        </div>
+    );
+}
