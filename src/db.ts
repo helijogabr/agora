72f9537e00f6caaaa2ed9bad3043754fd3906583
lia-lopes <ame.pistache@gmail.com>
2026-07-12 15:10:46 -0300

    fix:por favor

--- src/db.ts
+++ src/db.ts
@@ -6,8 +6,6 @@ import * as schema from "../db/schema";
 export * from "drizzle-orm/sql";
 export * from "../db/schema";
 
-console.log("INITIALIZING DATABASE CONNECTIONS...");
-
 export const kv: Redis = import.meta.env.DEV
   ? new (await import("ioredis-mock")).default({ db: 0 })
   : new (await import("ioredis")).default(REDIS_URL, { db: 0 });
