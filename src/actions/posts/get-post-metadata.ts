4f258d381c96a1aa8aced59a64270560e65ef782
Gabriel Moreira <gamocuso@gmail.com>
2026-07-14 16:06:53 -0300

    refactor(posts): extract post metadata actions

--- src/actions/posts/get-post-metadata.ts
+++ src/actions/posts/get-post-metadata.ts
@@ -2,11 +2,6 @@ import { defineAction } from "astro:actions";
 import { z } from "astro/zod";
 import { db } from "@/db";
 
-export { createPost } from "./posts/create-post";
-export { deletePost } from "./posts/delete-post";
-export { getPosts } from "./posts/get-posts";
-export { likePost } from "./posts/like-post";
-
 export const getPostTypes = defineAction({
   input: z.void(),
   handler: async () => {
---
+++ src/actions/posts/index.ts
@@ -0,0 +1,5 @@
+export { createPost } from "./create-post";
+export { deletePost } from "./delete-post";
+export { getPostTypes, getTags } from "./get-post-metadata";
+export { getPosts } from "./get-posts";
+export { likePost } from "./like-post";
